52d56d2d07212241baa249b5f053ab6242f7c78d
ilyawebdevelop <ilia966@tut.by>
2025-01-18 13:07:36 +0300

    sd

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import * as flsFunctions from "./modules/functions.js";
 import "./modules/jquery-3.7.1.min.js";
 import { Fancybox } from "./modules/fancybox.esm.js";
+import tippy from 'tippy.js';
 import "./modules/bootstrap.bundle.min.js";
 import './components.js';
 
@@ -9,6 +10,7 @@ flsFunctions.isWebp();
 Fancybox.bind("[data-fancybox]", {
   closeButton: false,
 });
+
 
 // Import swiper
 import Swiper, { Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar } from 'swiper';
@@ -50,7 +52,7 @@ document.querySelectorAll('.partnersSlider').forEach(n => {
     slidesPerView: 3,
     spaceBetween: 24,
     speed: 600,
-    navigation: {    
+    navigation: {
       nextEl: n?.closest('.partnersSliderW').querySelector('.navArrowNext'),
     },
     breakpoints: {
@@ -61,7 +63,7 @@ document.querySelectorAll('.partnersSlider').forEach(n => {
       768: {
         slidesPerView: 2,
         spaceBetween: 24,
-      },    
+      },
       1200: {
         slidesPerView: 3,
       },
@@ -98,6 +100,41 @@ document.querySelectorAll('.newsSlider').forEach(n => {
     },
   });
 });
+
+// Инициализация слайдера productMainSlider
+document.querySelectorAll('.productMainSlider').forEach(n => {
+  const mySwiperProductPage = new Swiper(n, {
+    slidesPerView: 1,
+    spaceBetween: 13,
+    speed: 600,
+    freeMode: false,
+    watchSlidesProgress: true,
+    mousewheel: false,
+    navigation: {
+      nextEl: n?.closest('.productSect').querySelector('.navArrowNext'),
+    },
+    thumbs: { // указываем на превью слайдер
+      swiper: {
+        el: document.querySelector('.productThumbSlider'),
+        slidesPerView: 5,
+        spaceBetween: 20,
+        direction: 'vertical',
+        speed: 600,
+        watchSlidesProgress: true,
+        breakpoints: {
+          0: {
+            spaceBetween: 5,
+          },
+          1200: {
+            spaceBetween: 15,
+          },
+        },
+      }
+    },
+  });
+});
+
+
 
 const mediaQuery991 = window.matchMedia('(max-width: 991px)');
 function findOffset(element) {
@@ -136,3 +173,32 @@ window.onload = function () {
   };
 
 };
+
+// loadmore
+let loadmoreArray = document.querySelectorAll('.loadmore');
+loadmoreArray.forEach(el => {
+  el.addEventListener('click', () => {
+    let parent = el.closest('.content-action');
+    let content = parent.querySelector('.content');
+    content.classList.add('show');
+    el.style.display = 'none';
+  });
+});
+
+// tippy
+let aboutProductItemBtn = document.querySelectorAll('.about-product-item-btn');
+let count = 0;
+aboutProductItemBtn.forEach(el => {
+  count++;
+  let template = el.closest('.about-product-item-wrapper').querySelector('.about-product-item');
+  let instanse = tippy(el, {
+    content: template.innerHTML,
+    allowHTML: true,
+    placement: 'top-start',
+    arrow: false,
+    onClickOutside: false,
+  });
+  if (count == 1) {
+    instanse.show();
+  }
+});
